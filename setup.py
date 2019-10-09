a32a66df81928964dace07e71f52224340ea0a06
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2019-10-08 23:32:08 -0700

    Updated the package list

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as req_file:
+    required_packages = [ pkg.rstrip() for pkg in req_file ]
+
 setup(
     name='pyxwm',
     version='0.0.1',
@@ -8,5 +11,6 @@ setup(
     author='Amlesh Sivanantham',
     author_email='samlesh@gmail.com',
     package_dir={'': 'src'},
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    install_requires=required_packages
 )
